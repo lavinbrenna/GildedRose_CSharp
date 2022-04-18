532a605c1dc763873756d2bb73f56e19672eaf50
Brenna Lavin <lavinbrenna@gmail.com>
2022-04-17 22:48:46 -0700

    initialize repository, upload solution

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+        public void UpdateQuality()
+        {
+            foreach(Item item in Items){
+                if(item.Name == "Sulfuras, Hand of Ragnaros"){
+                    item.Quality = 80;
+                }
+                if(item.Quality >= 0 && item.Quality <= 50)
+                {
+                    if(item.Name == "Aged Brie")
+                    {
+                        item.Quality += 1;
+                        if(item.Quality + 1 > 50)
+                        {
+                            item.Quality = 50;
+                        }
+                    }
+                    else if(item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if(item.SellIn < 0 && item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            item.Quality = 0;
+                        }
+                        else if(item.SellIn <= 10 && item.SellIn > 5)
+                        {
+                            item.Quality += 2;
+                            if(item.Quality + 2 > 50)
+                            {
+                                item.Quality = 50;
+                            }
+                        }
+                        else if(item.SellIn <= 5 && item.SellIn > 0)
+                        {
+                            item.Quality += 3;
+                            if(item.Quality + 3 > 50)
+                            {
+                                item.Quality = 50;
+                            }
+                        }
+                        else
+                        {
+                            item.Quality += 1;
+                            if(item.Quality +1 > 50)
+                            {
+                                item.Quality = 50;
+                            }
+                        }
+                    }
+                    else if(!item.Name.Contains("Conjured")){
+                        if(item.SellIn > 0){
+                            item.Quality -= 1;
+                            if(item.Quality - 1 < 0 )
+                            {
+                                item.Quality = 0;
+                            }
+                        }
+                        else{
+                            item.Quality -= 2;
+                            if(item.Quality -2 < 0)
+                            {
+                                item.Quality = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(item.SellIn > 0)
+                        {
+                            item.Quality -= 2;
+                            if(item.Quality - 2 < 0)
+                            {
+                                item.Quality = 0;
+                            }
+                        }
+                        else
+                        {
+                            item.Quality -= 4;
+                            if(item.Quality - 4 < 0)
+                            {
+                                item.Quality = 0;
+                            }
+                        }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,120 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+        }
+        [Fact]
+        public void FooQuality_DoesNotReturnNegative()
+        {
+            IList<Item> Items = new List<Item> 
+            {
+                new Item {Name = "foo", SellIn = -1, Quality = 0},
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
+        [Fact]
+        public void FooQuality_GoesDownDoublePastSellInDate()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "foo", SellIn = -2, Quality = 25}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(23, Items[0].Quality);
+        }
+        [Fact]
+        public void RagnarosQuality_DoesNotGoOver80()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(80, Items[0].Quality);
+        }
+
+        [Fact]
+        public void ConjuredQuality_QualityDropsDouble()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Conjured Mana", SellIn = 1, Quality = 49}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(47, Items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePasses_DropToZeroAfterConcert()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 40}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
+        [Fact]
+        public void BackstagePasses_IncreaseByThreeBeforeConcert()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 4, Quality = 40}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(43, Items[0].Quality);
+        }
+            [Fact]
+        public void BackstagePasses_IncreaseByTwoBeforeConcert()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 40}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(42, Items[0].Quality);
+        }
+        [Fact]
+        public void AgedBrie_QualityIncreases()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 3, Quality = 40}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(41, Items[0].Quality);
+        }
+
+        [Fact]
+        public void AgedBrie_QualityIncreasesEvenAfterSellIn()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = -3, Quality = 39}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(40, Items[0].Quality);
+        }
+    }
+}
